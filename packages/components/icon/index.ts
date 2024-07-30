2511343272173c3ddf079f0edc5a2bbf2b367f29
永遇乐i <3497645346@qq.com>
2024-07-30 18:23:17 +0800

    添加eslint+prettier

--- packages/components/icon/index.ts
+++ packages/components/icon/index.ts
@@ -1,16 +1,15 @@
 // 用来整合组件的 最终实现导出组件
 import _Icon from './src/icon.vue'
-import { withInstall}from '@zi-shui/utils/with-install'
+import { withInstall } from '@zi-shui/utils/with-install'
 
 const Icon = withInstall(_Icon)
 
 export default Icon
 export * from './src/icon'
 
-
 // 这里添加的类型，可以在模板中被解析
-declare module 'vue'{
-    export interface GolbalComponents{
-        ZIcon:typeof Icon
-    }
+declare module 'vue' {
+  export interface GolbalComponents {
+    ZIcon: typeof Icon
+  }
 }
--- packages/components/icon/src/icon.ts
+++ packages/components/icon/src/icon.ts
@@ -1,7 +1,7 @@
-import { ExtractPropTypes,PropType } from "vue";
+import { ExtractPropTypes, PropType } from 'vue'
 export const iconProps = {
-    color:String,
-    size:[Number,String] as PropType<number | string>
+  color: String,
+  size: [Number, String] as PropType<number | string>
 } as const
 
 export type IconProps = ExtractPropTypes<typeof iconProps>
